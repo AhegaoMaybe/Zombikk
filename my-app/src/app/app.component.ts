89c4fd5a889fbfd41dbdbf242f5d7cdade966e6f
231231212 <231231231@mail.ru>
2019-12-03 12:52:59 +0300

    Something

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,15 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  clicks:number = 0;
-  onChanged(increased){
-    if(increased){
-      this.clicks = this.clicks + 1;
-    }
-    else{
-      this.clicks = this.clicks - 1;
-    }
-  }
+  name:string;
+
 
 }
 
--- my-app/src/app/child/child.component.ts
+++ my-app/src/app/child/child.component.ts
@@ -1,12 +1,14 @@
-import { EventEmitter, Component, Output } from '@angular/core';
+import { EventEmitter, Component, Output, Input} from '@angular/core';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent {
-  @Output() onChanged = new EventEmitter<boolean>();
-  change(increased){
-    this.onChanged.emit(increased);
+  @Input() UserName: string;
+  @Output() UserNameChange = new EventEmitter<string>();
+  onNameChange(newName:string){
+    this.UserName = newName;
+    this.UserNameChange.emit(newName);
   }
 }
