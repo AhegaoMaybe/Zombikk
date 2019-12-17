2c6217ff686ba1b89e2dd1196b09c96ba09d2b16
3123123 <12312312312>
2019-12-17 12:31:31 +0300

    Control work1

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -5,7 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
-  name= 'Aleksey'
+export class AppComponent{
+  name:string;
+  clicks:number = 0;
+  onChanged(increased){
+    console.log('Вы вошли в ngOnChanged')
+    if(increased){
+      this.clicks = this.clicks + 1;
+    }
+    else{
+      this.clicks = this.clicks - 1;
+    }
+  }
+  ngOnInit() { 
+    console.log('Вы в ngOnInit')
+  }
+  
 }
