3fb87f5692ab110126d1488bb6dd1bae359a2363
3123123 <12312312312>
2019-12-17 10:37:28 +0300

    Control work

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name:string;
-
-  constructor(private route:Router){}
-  goHome(){
-    this.route.navigate([""])
-  }
+  title = 'my-app';
+  name= 'Aleksey'
 }
-
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,32 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import { ChildComponent } from './child/child.component';
-import { HomeComponent } from './routing/home/home.component';
-import { AboutComponent } from './routing/about/about.component';
-import { NotFoundComponent } from './routing/not-found/not-found.component';
-import { ItemComponent } from './routing/item/item.component';
-import { ItemDetailsComponent } from './routing/item/item-details/item-details.component';
-import { ItemStatsComponent } from './routing/item/item-stats/item-stats.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ChildComponent,
-    HomeComponent,
-    AboutComponent,
-    NotFoundComponent,
-    ItemComponent,
-    ItemDetailsComponent,
-    ItemStatsComponent
+    AppComponent
   ],
   imports: [
-    FormsModule,
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
