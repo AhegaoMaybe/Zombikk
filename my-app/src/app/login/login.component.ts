8a6f97b3164231de1a35e8d62d1ee1ec35a0c0ea
3123123 <12312312312>
2019-12-24 15:51:40 +0300

    LOGIN

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -8,21 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   login: string;
   password: string;
+  entered:boolean;
   constructor() { }
 
   ngOnInit() {
     this.login = 'admin1';
     this.password = 'admin1';
+    this.entered= false;
   }
 
   MakeLogin(){
     console.log(this.login);
     console.log(this.password);
-    if (this.login == 'admin' && this.password == 'admin'){
-      console.log('Вы вошли')
-
+    if (this.login === 'admin' && this.password === 'admin'){
+      console.log('Всё норм');
+      this.entered=true;
+    }
+    else{
+      const pass = localStorage.getItem('password');
+      const log = localStorage.getItem('login');
+    if(log === this.login && pass === this.password){
+      console.log('Всё хорошо вы вошли');
+      this.entered=true;
+    }
+    else{
+      this.entered = false;
+      localStorage.setItem('login', this.login);
+      localStorage.setItem('password', this.password);
     }
 
   }
-
+}
 }
