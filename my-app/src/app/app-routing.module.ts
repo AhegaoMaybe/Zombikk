9eedadb335c277d4b3881941efa415f8e3b19215
3123123 <12312312312>
2019-12-24 14:28:37 +0300

    Not ControlWork

---
+++ my-app/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const routes: Routes = [
+  {path: "login", component: LoginComponent },
+  {path: "main", component: MainComponent },
+  {path: "about", component: AboutComponent },
+  {path: "**", component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {path: "", component: MainComponent },
+  {path: "login", component: LoginComponent },
+  {path: "main", component: MainComponent },
+  {path: "about", component: AboutComponent },
+  {path: "**", component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    MainComponent,
+    AboutComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule
   ],
